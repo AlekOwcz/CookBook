393ed9682771753d5b8cadc44c9e3beb86f2f79a
AlekOwcz <96996586+AlekOwcz@users.noreply.github.com>
2023-04-26 01:07:28 +0200

    Add test button

--- app/src/main/java/com/example/cookbookapp/MainActivity.kt
+++ app/src/main/java/com/example/cookbookapp/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.cookbookapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun onShowDetail(view: View?) {
+        val intent = Intent(this, DetailActivity::class.java)
+        startActivity(intent)
     }
 }
